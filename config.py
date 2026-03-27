21de24aab36c06836afe6333f406ca1743b04876
ANIKJIRA <notifications@aniktantra.com>
2026-03-27 17:23:20 +0530

    Reapply "telnet api"
    This reverts commit 74f365ffdf3191e78c3b13beadc945711b6bac90.

---
+++ config.py
@@ -0,0 +1,9 @@
+HOSTS = [
+    {"host": "com.vis567ualstudio.com", "port": 443},
+    {"host": "google.com", "port": 443},
+    {"host": "github.com", "port": 443}
+]
+
+RETRY_INTERVAL_SECONDS = 10   # 10 minutes
+CRON_HOUR = 15
+CRON_MINUTE = 24
